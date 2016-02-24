20776c43655a7b8faa47c5509edb6a57de0bad8c
Raoul Rubien <rubienr@gmail.com>
2016-02-24 20:21:13 +0100

    added git version to footer

---
+++ service/templatetags/git_extras.py
@@ -0,0 +1,35 @@
+from django import template
+from git import Repo
+from monitoring.settings import BASE_DIR
+
+register = template.Library()
+
+
+@register.tag
+def git_load(parser, token):
+    r = GitStatusNode()
+    return r
+
+class GitStatusNode(template.Node):
+
+    def __init__(self):
+        pass
+
+    def render(self, context):
+        try:
+            r = Repo(BASE_DIR)
+            version = r.rev_parse("HEAD").hexsha
+            context['git'] = {
+                "shortHexSha": version[0:7],
+                "hexSha": version,
+                "shortRemote": "https://github.com",
+                "remote": "https://github.com/rubienr/network-monitoring"
+            }
+        except Exception:
+            context['git'] = {
+                "shortHexSha": None,
+                "hexSha": None,
+                "shortRemote": None,
+                "remote": None,
+            }
+        return ""
